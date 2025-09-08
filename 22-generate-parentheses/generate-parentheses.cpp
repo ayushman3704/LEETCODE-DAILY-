81fd4f7652a39512704878c2544f4d21adb35512
ayushman3704 <ayushman3704@gmail.com>
2025-09-08 20:40:55 +0530

    Time: 3 ms (71.42%) | Memory: 13.4 MB (73.93%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,44 +1,50 @@
-class Solution { // using recursion and backtracking
-                // T.C O(2^n * 2n)
+class Solution { // using recursion and backtracking. approach 2 optimal approach
+                // T.C O(2^n)
                 // s.c O(2n)
 public:
     vector<string> result;
-    // func to check valid parentheses
-    bool isValid(string &curr){
-        int count = 0;
+    // // func to check valid parentheses
+    // bool isValid(string &curr){
+    //     int count = 0;
 
-        for(char ch : curr){
-            if(ch == '(')
-            count++;
+    //     for(char ch : curr){
+    //         if(ch == '(')
+    //         count++;
 
-            else
-            count--;
+    //         else
+    //         count--;
 
-            if(count < 0)
-            return false;
-        }
-        return count == 0;
-    }
-    void solve(string &curr, int n){
+    //         if(count < 0)
+    //         return false;
+    //     }
+    //     return count == 0;
+    // }
+    void solve(string &curr, int n, int open, int close){
         if(curr.length() == 2*n){
-            if(isValid(curr))
             result.push_back(curr);
 
             return;
         }
 
-        curr.push_back('(');
-        solve(curr, n);
-        curr.pop_back(); 
+        if(open < n){
+            curr.push_back('(');
+            solve(curr, n, open+1, close);
+            curr.pop_back();
+        } 
 
-        curr.push_back(')');
-        solve(curr, n);
-        curr.pop_back();
+        if(close < open){
+            curr.push_back(')');
+            solve(curr, n, open, close+1);
+            curr.pop_back();
+        }
 
     }
     vector<string> generateParenthesis(int n) {
+        int open = 0;
+        int close = 0;
+
         string str = "";
-        solve(str, n);
+        solve(str, n, open, close);
 
         return result;
     }
