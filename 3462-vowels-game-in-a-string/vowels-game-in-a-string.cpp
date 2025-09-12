9e63e8bbd6c2ad6be4f7e2cd2501b56b8b5d0221
ayushman3704 <ayushman3704@gmail.com>
2025-09-12 10:46:28 +0530

    Time: 23 ms (7.09%) | Memory: 19.6 MB (11.38%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        string temp;
+        // string temp;
 
-        for(char ch : s){
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-            temp.push_back(ch);
-        } 
+        // for(char ch : s){
+        //     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        //     temp.push_back(ch);
+        // } 
 
-        // if((temp.length() >= 1)|| temp.length() == s.length())
+        // // if((temp.length() >= 1)|| temp.length() == s.length())
+        // // return true;
+
+        // // else if(temp.length() == 1 || temp.length() % 2 == 1)
+        // // return true;
+
+        // // else if(temp.length() == 0 || temp.length() % 2 == 1)
+
+        // if(temp.length() == 0)
+        // return false;
+
         // return true;
 
-        // else if(temp.length() == 1 || temp.length() % 2 == 1)
-        // return true;
 
-        // else if(temp.length() == 0 || temp.length() % 2 == 1)
 
-        if(temp.length() == 0)
-        return false;
-
-        return true;
+        return any_of(s.begin(), s.end(), [](char ch){
+            return string("aeiou").find(ch) != string::npos;
+        });
     }
 };
