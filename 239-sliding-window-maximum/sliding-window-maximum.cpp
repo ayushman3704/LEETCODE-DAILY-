5f13b76977733998b436002895d5cd363803e549
ayushman3704 <ayushman3704@gmail.com>
2025-09-23 15:51:15 +0530

    Time: 55 ms (22.00%) | Memory: 153.2 MB (14.74%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,30 +1,22 @@
-class Solution { // sliding window. Monotonic increasing/decreasing pattern.
+class Solution { // using max-heap priority queue. APPROACH-2
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq; // it stores {nums[i], i}
+        vector<int> result;
+
         int n = nums.size();
 
-        deque<int> deq;
+        for(int i = 0; i<n; i++) {
+            
+            //remove the max elements which are out of window size. ensure the size of window should be of k
+            while(!pq.empty() && pq.top().second <= i-k)
+                pq.pop();
+            
+            pq.push({nums[i], i}); //we will always find the max element at top
 
-        int i = 0;
-        vector<int> res;
-
-        while(i < n){
-            // step- 1. window size not be greater than k
-            while(!deq.empty() && deq.front() <= i - k){
-                deq.pop_front();
-            }
-            // step- 2. greater element should be at the front
-            while(!deq.empty() && nums[i] > nums[deq.back()]){
-                deq.pop_back();
-            }
-            // step- 3. if step- 1 and 2 satisfy then push element into the deque
-            deq.push_back(i);
-            // step- 4. if window size is valid then push front element of the deque into res
-            if(i >= k - 1){
-                res.push_back(nums[deq.front()]);
-            }
-            i++;
+            if(i >= k-1) // if the window size is valid then push the top element(greater element) into the result
+                result.push_back(pq.top().first);
         }
-        return res;
+        return result;
     }
 };
