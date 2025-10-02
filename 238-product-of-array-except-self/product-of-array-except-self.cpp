be0da6096c3d6cbfa52ce3368bcaabc04dfdd446
ayushman3704 <ayushman3704@gmail.com>
2025-10-02 10:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (58.27%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,28 +1,28 @@
-class Solution { // this approach: t.c O(n)  s.c O(n) (without using division operation)
+class Solution { // this approach: t.c O(n)  s.c O(1) (without using division operation)
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
 
-        int left[n]; // left array to store product of left sides element of index i
-        int right[n]; // right array to store product of right sides elements of index i
+        // int left[n]; // left array to store product of left sides element of index i
+        // int right[n]; // right array to store product of right sides elements of index i
 
-        left[0] = 1; 
-        right[n-1] = 1;
+        // left[0] = 1; 
+        // right[n-1] = 1;
 
-        for(int i = 1; i < n; i++){
-            left[i] = left[i - 1] * nums[i - 1];
+        vector<int> result(n);
+
+        result[0] = 1;
+
+        for(int i = 1; i < n; i++){ // traverse and store left product
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        
+        int right_product = 1;
+        for(int i = n-1; i >= 0; i--){
+            result[i] = result[i] * right_product; // left product * right product
+            right_product *= nums[i]; // update right product 
         }
 
-        for(int i = n-2; i >= 0; i--){
-            right[i] = right[i+1] * nums[i+1];
-        }
-
-        vector<int> res(n);
-
-        for(int i = 0; i < n; i++){
-            res[i] = left[i] * right[i];
-        }
-
-        return res;
+        return result;
     }
 };
