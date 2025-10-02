c430f66d3f90eb241325e4e4e5543433371d2641
ayushman3704 <ayushman3704@gmail.com>
2025-10-02 20:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (79.98%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int drink = 0;
-        
-        while(numBottles >= numExchange){
-            drink += numExchange;
-            numBottles -= numExchange;
-
-            numBottles ++;
-        }
-        drink += numBottles;
-
-        return drink;
+        return numBottles + (numBottles - 1)/(numExchange - 1);
     }
 };
