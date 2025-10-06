5e9038ed3ea1e2a567bcc2f0c8c0f95d23bf975c
ayushman3704 <ayushman3704@gmail.com>
2025-10-06 19:57:54 +0530

    Time: 7 ms (20.08%) | Memory: 9.8 MB (48.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,25 +1,9 @@
-class Solution {
-public:
+class Solution { // using sorting
+public: 
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> mp;
+       sort(begin(s), end(s));
+       sort(begin(t), end(t));
 
-        for(char & ch : s){
-            mp[ch-'a']++;
-        }
-
-        for(char & ch : t){
-            if(mp.find(ch - 'a') != mp.end()){
-                mp[ch-'a']--;
-
-                if(mp[ch-'a'] == 0)
-                mp.erase(ch-'a');
-            }else{
-                return false;
-            }
-        }
-        if(mp.size() == 0)
-        return true;
-
-        return false;
+       return s == t;
     }
 };
