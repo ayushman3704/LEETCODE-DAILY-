34be29b125ecf588793567baa182fc4608309445
ayushman3704 <ayushman3704@gmail.com>
2025-10-06 20:29:26 +0530

    Time: 20 ms (40.80%) | Memory: 27.2 MB (24.79%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,5 +1,22 @@
 class Solution { // using sorting
 public:
+    string generate(string &word){
+        int arr[26] = {0};
+
+        for(char &ch : word){
+            arr[ch-'a']++;
+        }
+
+        string new_word = "";
+        for(int i = 0; i < 26; i++){
+            int freq = arr[i];
+
+            if(freq > 0){
+                new_word += string(freq, i + 'a'); 
+            }
+        }
+        return new_word;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
         
@@ -8,11 +25,11 @@ public:
         unordered_map<string, vector<string>> mp;
 
         for(int i = 0; i < n; i++){
-            string temp = strs[i];
+            string word = strs[i];
 
-            sort(begin(temp), end(temp));
+            string new_word = generate(word);
 
-            mp[temp].push_back(strs[i]);
+            mp[new_word].push_back(word);
         }
 
         for(auto it : mp){
