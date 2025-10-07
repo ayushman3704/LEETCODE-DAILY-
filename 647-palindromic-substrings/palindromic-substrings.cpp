691334dce6c71709c77842dab7a7ade682856178
ayushman3704 <ayushman3704@gmail.com>
2025-10-07 21:11:10 +0530

    Time: 31 ms (32.38%) | Memory: 12.9 MB (43.46%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,16 +1,22 @@
 class Solution { // using recursion
 public:
+    int t[1001][1001];
     bool check(int i, int j, string &s){
         if(i > j)
         return true;
 
+        if(t[i][j] != -1)
+        return t[i][j];
+
         if(s[i] == s[j]){
-            return check(i+1, j-1, s);
+            return t[i][j] = check(i+1, j-1, s);
         }
-        return false;
+        return t[i][j] = false;
     }
     int countSubstrings(string s) {
         int n = s.length();
+
+        memset(t, -1, sizeof(t));
 
         int count = 0;
 
