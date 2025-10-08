538dc0394ce82d1ccc4c9a9d9af9cd8b273a49aa
ayushman3704 <ayushman3704@gmail.com>
2025-10-08 08:56:42 +0530

    Time: 32 ms (29.77%) | Memory: 11.1 MB (53.73%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,28 +1,25 @@
-class Solution { // using recursion
+class Solution { // using bottom up approach. Blueprint.
 public:
-    int t[1001][1001];
-    bool check(int i, int j, string &s){
-        if(i > j)
-        return true;
-
-        if(t[i][j] != -1)
-        return t[i][j];
-
-        if(s[i] == s[j]){
-            return t[i][j] = check(i+1, j-1, s);
-        }
-        return t[i][j] = false;
-    }
     int countSubstrings(string s) {
         int n = s.length();
 
-        memset(t, -1, sizeof(t));
+        vector<vector<bool>> t(n, vector<bool>(n, false));
 
         int count = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(check(i, j, s))
+        for(int l = 1; l <= n; l++){
+            for(int i = 0; i+l-1 < n ; i++){
+                int j = i+l-1;
+
+                if(i == j){
+                    t[i][j] = true;
+                }else if(i+1 == j){
+                    t[i][j] = (s[i] == s[j]);
+                }else{
+                    t[i][j] = (s[i] == s[j] && t[i+1][j-1]);
+                }
+
+                if(t[i][j] == true)
                 count++;
             }
         }
