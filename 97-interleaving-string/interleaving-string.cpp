636644a31c4860f9e014ef4b9f533c4b4e9faa4b
ayushman3704 <ayushman3704@gmail.com>
2025-10-10 19:53:52 +0530

    Time: 4 ms (53.92%) | Memory: 9 MB (83.30%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,40 +1,40 @@
-class Solution { // using recursion + memoization
+class Solution { // using recursion + memoization. 2-D DP
 public:
-    int t[101][101][201];
+    int t[101][101];
 
-    bool solve(int i, int j, int k, string& s1, string& s2, string& s3){
+    bool solve(int i, int j, string& s1, string& s2, string& s3){
         int n = s1.length();
         int m = s2.length();
         int N = s3.length();
 
-        if(i == n && j == m && k == N)
+        if(i == n && j == m && i+j == N)
         return true;
 
-        if(k >= N)
+        if(i+j >= N)
         return false;
 
-        if(t[i][j][k] != -1)
-        return t[i][j][k];
+        if(t[i][j] != -1)
+        return t[i][j];
 
         bool result = false;
 
-        if(s1[i] == s3[k]){
-            result = solve(i+1, j, k+1, s1, s2, s3);
+        if(s1[i] == s3[i+j]){
+            result = solve(i+1, j, s1, s2, s3);
         }
 
         if(result == true) // optional, slight optimization
-        return t[i][j][k] = result;
+        return t[i][j] = result;
 
-        if(s2[j] == s3[k]){
-            result = solve(i, j+1, k+1, s1, s2, s3);
+        if(s2[j] == s3[i+j]){
+            result = solve(i, j+1, s1, s2, s3);
         }
 
-        return t[i][j][k] = result;
+        return t[i][j] = result;
     }
     bool isInterleave(string s1, string s2, string s3) {
         memset(t, -1, sizeof(t));
 
-        // i -> pointing to s1, j -> pointing to s2, k -> pointing to s3
-        return solve(0, 0, 0, s1, s2, s3);
+        // i -> pointing to s1, j -> pointing to s2, i+j -> pointing to s3
+        return solve(0, 0, s1, s2, s3);
     }
 };
