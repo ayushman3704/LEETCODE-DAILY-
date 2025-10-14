69108c460413069bcdf024689c71ff9617903b93
ayushman3704 <ayushman3704@gmail.com>
2025-10-14 19:27:36 +0530

    Time: 383 ms (27.00%) | Memory: 90 MB (5.08%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,26 @@
-class Solution { // using recursion. we have two options at every index- plus or minus like take or not take.
+class Solution { // memoization using map. 
+                 // we have two options at every index- plus or minus like take or not take.
 public:
-    int solve(vector<int>& nums, int i, int target, int currSum){
+    int solve(vector<int>& nums, int i, int target, int currSum, unordered_map<string, int>& mp){
         if(i == nums.size()){
             if(currSum == target)
             return 1;
             else
             return 0;
         }
+        string key = to_string(i) + "_" + to_string(currSum);
+        if(mp.count(key)){
+            return mp[key];
+        }
 
-        int plus = solve(nums, i+1, target, currSum+nums[i]);
-        int mins = solve(nums, i+1, target, currSum-nums[i]);
+        int plus = solve(nums, i+1, target, currSum+nums[i], mp);
+        int mins = solve(nums, i+1, target, currSum-nums[i], mp);
 
-        return plus+mins;
+        return mp[key] = plus+mins;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        return solve(nums, 0, target, 0);
+        unordered_map<string, int> mp;
+
+        return solve(nums, 0, target, 0, mp);
     }
 };
