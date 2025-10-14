6723bf5c90a27afe823630965161c4fb302b4eeb
ayushman3704 <ayushman3704@gmail.com>
2025-10-14 19:33:54 +0530

    Time: 16 ms (49.46%) | Memory: 26.3 MB (22.55%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,31 @@
-class Solution { // memoization using map. 
+class Solution { // memoization 2D vector. 
                  // we have two options at every index- plus or minus like take or not take.
 public:
-    int solve(vector<int>& nums, int i, int target, int currSum, unordered_map<string, int>& mp){
-        if(i == nums.size()){
+    int S;
+    int n;
+    int solve(vector<int>& nums, int i, int target, int currSum, vector<vector<int>>& t){
+        if(i == n){
             if(currSum == target)
             return 1;
             else
             return 0;
         }
-        string key = to_string(i) + "_" + to_string(currSum);
-        if(mp.count(key)){
-            return mp[key];
+        if(t[i][currSum+S] != -1){
+            return t[i][currSum+S];
         }
 
-        int plus = solve(nums, i+1, target, currSum+nums[i], mp);
-        int mins = solve(nums, i+1, target, currSum-nums[i], mp);
+        int plus = solve(nums, i+1, target, currSum+nums[i], t);
+        int mins = solve(nums, i+1, target, currSum-nums[i], t);
 
-        return mp[key] = plus+mins;
+        return t[i][currSum+S] = plus+mins;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string, int> mp;
+        S = accumulate(begin(nums), end(nums), 0); // calculate sum and store in S
 
-        return solve(nums, 0, target, 0, mp);
+        n = nums.size();
+
+        vector<vector<int>> t(n+1, vector<int>(2*S+1, -1));
+
+        return solve(nums, 0, target, 0, t);
     }
 };
