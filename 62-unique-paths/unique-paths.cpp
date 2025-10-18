87c1e328fd2a979a58a17b1c5ef414235db16006
ayushman3704 <ayushman3704@gmail.com>
2025-10-18 12:41:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (12.50%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,24 +1,24 @@
-class Solution { // recursion + memoization. Pattern- "Grid / Matrix DP"
+class Solution { // bottum up. Pattern- "Grid / Matrix DP"
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>> &t){
-        if(i == m-1 && j == n-1) // base case
-        return 1;
-
-        if(i < 0 || i >= m || j < 0 || j >= n) // check for valid path (within grid)
-        return 0;
-
-        if(t[i][j] != -1)
-        return t[i][j];
-
-        
-        int right = solve(i, j+1, m, n, t);
-        int down = solve(i+1, j, m, n, t);
-        
-        return t[i][j] = right + down;
-    }
+    
     int uniquePaths(int m, int n) {
-        vector<vector<int>> t(m+1, vector<int>(n+1, -1));
+        vector<vector<int>> t(m, vector<int>(n));
 
-        return solve(0, 0, m, n, t);
+        t[0][0] = 1;
+
+        for(int col = 1; col < n; col++){
+            t[0][col] = 1;
+        }
+
+        for(int row = 1; row < m; row++){
+            t[row][0] = 1;
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                t[i][j] = t[i-1][j] + t[i][j-1];
+            }
+        }
+        return t[m-1][n-1];
     }
 };
