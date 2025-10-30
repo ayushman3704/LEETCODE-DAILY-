ca07a71ff63ef342c6774de1c22c51417807a8f6
ayushman3704 <ayushman3704@gmail.com>
2025-10-30 22:53:45 +0530

    Time: 6 ms (17.35%) | Memory: 77 MB (65.87%) - LeetSync

---
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -0,0 +1,26 @@
+//Simple Approach - Keeping track of prev diff
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+         int n = target.size();
+
+        long long res = 0;
+
+        int curr = 0;
+        int prev = 0;
+
+        for(int i = 0; i < n; i++) {
+            curr = target[i];
+
+            if(abs(curr) > abs(prev)) {
+                res += abs(curr - prev);
+            }
+
+            prev = curr;
+        }
+
+        return res;
+    }
+};
