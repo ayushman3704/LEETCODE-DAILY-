548d555b23f4f5efe9ab34249b7eeb823463ae1f
ayushman3704 <ayushman3704@gmail.com>
2025-12-18 12:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (87.79%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,36 +1,14 @@
-class Solution { // bottom-up (using dp approach)
+class Solution { // Greedy approach
 public:
-    // bool solve(vector<int> &nums, int n, int ind, vector<int> &dp){
-    //     if(ind == n-1)
-    //     return true;
-
-    //     // if(ind >= n)
-    //     // return true;
-
-    //     if(dp[ind] != -1)
-    //     return dp[ind];
-
-    //     for(int i = 1; i <= nums[ind]; i++){
-    //         if(solve(nums, n, ind + i, dp) == true)
-    //         return dp[ind] = true;
-    //     }
-    //     return dp[ind] = false;
-    // }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
+        int maxReach = 0;
+        for(int i = 0; i < n; i++){
+            if(i > maxReach)
+            return false;
 
-        vector<bool> dp(n, false);
-
-        dp[0] = true;
-        for(int i = 1; i < n; i++){
-            for(int j = i-1; j >= 0; j--){
-                if(dp[i] == true)
-                break;
-                if(dp[j] == true && j + nums[j] >= i){
-                    dp[i] = true;
-                }
-            }
+            maxReach = max(maxReach, i + nums[i]);
         }
-        return dp[n-1];
+        return true;
     }
 };
