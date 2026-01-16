df7578c2bca7d0d963cd36f012a6998d0b8f604b
ayushman3704 <ayushman3704@gmail.com>
2026-01-16 19:43:54 +0530

    Time: 71 ms (14.48%) | Memory: 87.9 MB (12.48%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,22 +1,15 @@
-class Solution { // Pattern - Hashing / Frequency | Hint: Floyd tortoise-hare on index graph
+class Solution { // using hashSet.
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[0];
+        
+        unordered_set<int> seen;
 
-        slow = nums[slow]; // move by one step
-        fast = nums[nums[fast]]; // move by two steps
+        for(int x : nums){
+            if(seen.count(x))
+            return x;
 
-        while(slow != fast){ // detect cycle 
-            slow = nums[slow];
-            fast = nums[nums[fast]];
+            seen.insert(x);
         }
-
-        slow = nums[0]; // start from beigning
-        while(slow != fast){ // find starting point of cycle (duplicate element)
-            slow = nums[slow]; // move pointer by one step
-            fast = nums[fast]; // move pointer by one step
-        }
-        return fast;
+        return -1;
     }
 };
