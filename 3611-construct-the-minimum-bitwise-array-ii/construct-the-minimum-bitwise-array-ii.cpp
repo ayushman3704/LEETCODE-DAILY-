8b3deb8d9f25168670ccff6bcdca248cdfd39204
ayushman3704 <ayushman3704@gmail.com>
2026-01-21 16:01:40 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (83.50%) - LeetSync

---
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+
+        for(int i = 0; i < n; i++){
+            int x = nums[i];
+
+            if(x == 2){ // special case
+                ans[i] = -1;
+                continue;
+            }
+
+            // find first rightmost 0 bit
+            int bit = 0;
+            while(x & (1 << bit)){
+                bit++;
+            }
+
+            ans[i] = x - (1 << (bit - 1));
+        }
+        return ans;
+    }
+};
