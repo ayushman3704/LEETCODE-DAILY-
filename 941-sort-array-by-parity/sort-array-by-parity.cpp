9acc4a7843a4161b81be77cb451e51c5c5b05857
ayushman3704 <ayushman3704@gmail.com>
2026-02-01 08:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (67.41%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,11 +1,28 @@
-class Solution { // using custom sorting or comparator functioon
+class Solution { 
 public:
-    static bool comparator(int &a, int &b){
-        return (a % 2 < b % 2);
-    }
-    vector<int> sortArrayByParity(vector<int>& nums) {
-        sort(begin(nums), end(nums), comparator);
 
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int i = 0;
+        int j = n-1;
+
+        while(i < j){
+
+            if(nums[i]%2 == 0 && nums[j]%2 != 0){
+                i++;
+                j--;
+            }else if(nums[i]%2 != 0 && nums[j]%2 == 0){
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }else if(nums[i]%2 != 0 && nums[j]%2 != 0){
+                j--;
+            }else if(nums[i]%2 == 0 && nums[j]%2 == 0){
+                i++;
+            }
+        }
         return nums;
     }
 };
