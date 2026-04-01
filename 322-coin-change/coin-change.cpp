68ec69fc1c591688f74895b68196b8a2d5d91919
ayushman3704 <ayushman3704@gmail.com>
2026-04-01 15:26:27 +0530

    Time: 18 ms (82.47%) | Memory: 18 MB (70.05%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution { // using Tabulation. "UNBOUNDED KNAPSACK PROBLEM"
+public:
+
+    int coinChange(vector<int>& coins, int amount) {
+        
+        vector<int> dp(amount + 1, 1e9);
+
+        dp[0] = 0;
+        
+        for(int i = 1; i <= amount; i++){
+
+            for(int c : coins){
+
+                if(i - c >= 0){
+                    dp[i] = min(dp[i], 1 + dp[i - c]);
+                }
+            }
+        }
+
+        if(dp[amount] >= 1e9)
+        return -1;
+
+        return dp[amount];
+    }
+};
