e294af7045b17f46501a1acac676e73617ee7a21
ayushman3704 <ayushman3704@gmail.com>
2026-04-01 16:31:14 +0530

    Time: 175 ms (19.94%) | Memory: 17.5 MB (53.89%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,61 +1,36 @@
-class Solution { // memoized approach 
+class Solution { // Tabulation Approach
 public:
-    // void solve(vector<int>& candidates, int ind, vector<int> &temp, vector<vector<int>> &res, int target){
-    //     if(ind == candidates.size() || target < 0){
-    //         return;
-    //     }
-    //     if(target == 0){
-    //         // if(st.find(temp) == st.end()){
-    //             res.push_back({temp});
-    //             // st.insert(temp);
-    //         // }
-    //         return;
-    //     }
-
-    //     temp.push_back(candidates[ind]); // take
-    //     solve(candidates, ind+1, temp, res, target-candidates[ind]); // take and increase index(ind)
-    //     //solve(candidates, ind, temp, res, target-candidates[ind]); // takde and don't increase index(ind). take element repeatedly
-    //     temp.pop_back(); // not take
-
-    //     solve(candidates, ind+1, temp, res, target); // explore 
-    // }
-
-    int t[201][20001];
-    bool solve(vector<int>& nums, int i, int x) {
-        if(x == 0) {
-            return true;
-        }
-
-        if(i >= nums.size()) {
-            return false;
-        }
-
-        if(t[i][x] != -1) {
-            return t[i][x];
-        }
-
-        bool take = false;
-        if(nums[i] <= x) {
-            take = solve(nums, i+1, x - nums[i]);
-        }
-
-        bool not_take = solve(nums, i+1, x);
-
-        return t[i][x] = take || not_take;
-    }
-
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
         
-        int S = accumulate(begin(nums), end(nums), 0);
+        int n = nums.size();
 
-        if(S%2 != 0) {
-            return false;
-        }  
-        memset(t, -1, sizeof(t));
-        //vector<vecyot<int>> t(n+1, vector<int>(x+1, -1))
-        int x = S/2;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
 
-        return solve(nums, 0, x);
+        if(sum % 2 != 0)
+        return false;
+
+        int target = sum / 2;
+
+        vector<vector<bool>> dp(n+1, vector<bool> (target + 1, false));
+
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = true;
+        }
+
+        for(int i = 1; i <= n; i++){
+            
+            for(int t = 1; t <= target; t++){
+
+                bool notTake = dp[i-1][t];
+
+                bool take = false;
+                if(nums[i-1] <= t){
+                    take = dp[i-1][t-nums[i-1]];
+                }
+
+                dp[i][t] = take || notTake;
+            }
+        }
+        return dp[n][target];
     }
 };
