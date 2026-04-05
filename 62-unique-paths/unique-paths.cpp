d979cd526ee0ac5634be339d61fd5e349954bacd
ayushman3704 <ayushman3704@gmail.com>
2026-04-06 04:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.34%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -6,17 +6,24 @@ public:
 
         t[0][0] = 1;
 
-        for(int col = 1; col < n; col++){
-            t[0][col] = 1;
-        }
+        // for(int col = 1; col < n; col++){
+        //     t[0][col] = 1;
+        // }
 
-        for(int row = 1; row < m; row++){
-            t[row][0] = 1;
-        }
+        // for(int row = 1; row < m; row++){
+        //     t[row][0] = 1;
+        // }
 
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                t[i][j] = t[i-1][j] + t[i][j-1];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                if(i == 0 && j == 0)
+                continue;
+
+                int up = (i > 0) ? t[i-1][j] : 0;
+                int left = (j > 0) ? t[i][j-1] : 0;
+
+                t[i][j] = up + left;
             }
         }
         return t[m-1][n-1];
