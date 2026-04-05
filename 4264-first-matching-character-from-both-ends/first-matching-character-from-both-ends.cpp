7b4dd86d1db598f5f9304c9fbd7232f3a2b74077
ayushman3704 <ayushman3704@gmail.com>
2026-04-05 19:34:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (92.80%) - LeetSync

---
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        
+
+        int n = s.length();
+
+        int i = 0;
+        int j = n-1;
+
+        while(i <= j){
+
+            if(s[i] == s[j])
+            return i;
+
+            i++;
+            j--;
+        }
+
+        return -1;
+    }
+};
